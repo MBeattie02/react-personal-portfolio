ad42ecc9a5b78f73278d7e2261a05607b7660194
MBeattie02 <matthewbeattie22@gmail.com>
2025-01-27 19:18:29 +0000

    fix css on education page

--- src/components/Education/timelineElements.js
+++ src/components/Education/timelineElements.js
@@ -6,13 +6,133 @@ let timelineElements = [
     type: 'EDUCATION',
     description: (
       <span>
-        <h6>3rd Year Computer Science Modules </h6>
-        <ul>
-          <li>Cloud Computing</li>
-          <li>Concurrent Programming</li>
-          <li>Software Testing</li>
-          <li>Network Security</li>
-          <li>Computer Science Project</li>
+        <h6
+          style={{
+            color: '#2F4F4F',
+            marginBottom: '15px',
+            fontSize: '1.1rem',
+            fontWeight: '600',
+          }}
+        >
+          3rd Year Computer Science Modules
+        </h6>
+        <ul
+          style={{
+            listStyle: 'none',
+            padding: '0 15px',
+            margin: '0',
+          }}
+        >
+          <li
+            style={{
+              marginBottom: '8px',
+              position: 'relative',
+              paddingLeft: '20px',
+              lineHeight: '1.4',
+            }}
+          >
+            <span
+              style={{
+                position: 'absolute',
+                left: '0',
+                content: '""',
+                width: '6px',
+                height: '6px',
+                backgroundColor: '#4CAF50',
+                borderRadius: '50%',
+                top: '8px',
+              }}
+            ></span>
+            Cloud Computing
+          </li>
+          <li
+            style={{
+              marginBottom: '8px',
+              position: 'relative',
+              paddingLeft: '20px',
+              lineHeight: '1.4',
+            }}
+          >
+            <span
+              style={{
+                position: 'absolute',
+                left: '0',
+                content: '""',
+                width: '6px',
+                height: '6px',
+                backgroundColor: '#4CAF50',
+                borderRadius: '50%',
+                top: '8px',
+              }}
+            ></span>
+            Concurrent Programming
+          </li>
+          <li
+            style={{
+              marginBottom: '8px',
+              position: 'relative',
+              paddingLeft: '20px',
+              lineHeight: '1.4',
+            }}
+          >
+            <span
+              style={{
+                position: 'absolute',
+                left: '0',
+                content: '""',
+                width: '6px',
+                height: '6px',
+                backgroundColor: '#4CAF50',
+                borderRadius: '50%',
+                top: '8px',
+              }}
+            ></span>
+            Software Testing
+          </li>
+          <li
+            style={{
+              marginBottom: '8px',
+              position: 'relative',
+              paddingLeft: '20px',
+              lineHeight: '1.4',
+            }}
+          >
+            <span
+              style={{
+                position: 'absolute',
+                left: '0',
+                content: '""',
+                width: '6px',
+                height: '6px',
+                backgroundColor: '#4CAF50',
+                borderRadius: '50%',
+                top: '8px',
+              }}
+            ></span>
+            Network Security
+          </li>
+          <li
+            style={{
+              marginBottom: '8px',
+              position: 'relative',
+              paddingLeft: '20px',
+              lineHeight: '1.4',
+            }}
+          >
+            <span
+              style={{
+                position: 'absolute',
+                left: '0',
+                content: '""',
+                width: '6px',
+                height: '6px',
+                backgroundColor: '#4CAF50',
+                borderRadius: '50%',
+                top: '8px',
+              }}
+            ></span>
+            Computer Science Project
+          </li>
           <h5>
             Code-Review-as-an-Educational-Service: An Education-oriented
             Software-as-a-Service Tool for Java Code Review
@@ -29,50 +149,58 @@ let timelineElements = [
             minimising configurations/operations and maximising the easiness to
             use the tool for beginners.
           </p>
-          <button
-            className="btn"
+          <div
             style={{
-              backgroundColor: '#4CAF50',
-              color: 'white',
-              padding: '10px 20px',
-              textAlign: 'center',
-              textDecoration: 'none',
-              display: 'inline-block',
-              fontSize: '14px',
-              margin: '5px 2px',
-              cursor: 'pointer',
-              border: 'none',
-              borderRadius: '4px',
+              display: 'flex',
+              gap: '10px',
+              justifyContent: 'flex-start',
             }}
-            onClick={() =>
-              window.open('https://github.com/MBeattie02/code_review_tool')
-            }
           >
-            View Project On Github
-          </button>
-          <button
-            className="btn"
-            style={{
-              backgroundColor: '#4CAF50',
-              color: 'white',
-              padding: '10px 20px',
-              textAlign: 'center',
-              textDecoration: 'none',
-              display: 'inline-block',
-              fontSize: '14px',
-              margin: '5px 2px',
-              cursor: 'pointer',
-              border: 'none',
-              borderRadius: '4px',
-            }}
-            onClick={() =>
-              window.open(
-                'https://www.sciencedirect.com/science/article/pii/S2352711025000159'
-              )
-            }
-          >
-            View Published Paper On Science Direct
-          </button>
+            <button
+              className="btn"
+              style={{
+                backgroundColor: '#4CAF50',
+                color: 'white',
+                padding: '10px 20px',
+                textAlign: 'center',
+                textDecoration: 'none',
+                display: 'inline-block',
+                fontSize: '14px',
+                cursor: 'pointer',
+                border: 'none',
+                borderRadius: '4px',
+                whiteSpace: 'nowrap',
+              }}
+              onClick={() =>
+                window.open('https://github.com/MBeattie02/code_review_tool')
+              }
+            >
+              View Project On Github
+            </button>
+            <button
+              className="btn"
+              style={{
+                backgroundColor: '#4CAF50',
+                color: 'white',
+                padding: '10px 20px',
+                textAlign: 'center',
+                textDecoration: 'none',
+                display: 'inline-block',
+                fontSize: '14px',
+                cursor: 'pointer',
+                border: 'none',
+                borderRadius: '4px',
+                whiteSpace: 'nowrap',
+              }}
+              onClick={() =>
+                window.open(
+                  'https://www.sciencedirect.com/science/article/pii/S2352711025000159'
+                )
+              }
+            >
+              View Published Paper
+            </button>
+          </div>
           <h6>Overall Average : 75</h6>
         </ul>
       </span>
@@ -87,13 +215,54 @@ let timelineElements = [
     type: 'EDUCATION',
     description: (
       <span>
-        <h6>2nd Year Computer Science Modules </h6>
-        <ul>
-          <li>Cyber Security Fundamentals</li>
-          <li>Software Engineering and Systems Development</li>
-          <li>Data Structures and Algorithms</li>
-          <li>Theory of Computation</li>
-          <li>Professional & Transfer Skills</li>
+        <h6
+          style={{
+            color: '#2F4F4F',
+            marginBottom: '15px',
+            fontSize: '1.1rem',
+            fontWeight: '600',
+          }}
+        >
+          2nd Year Computer Science Modules
+        </h6>
+        <ul
+          style={{
+            listStyle: 'none',
+            padding: '0 15px',
+            margin: '0',
+          }}
+        >
+          {[
+            'Cyber Security Fundamentals',
+            'Software Engineering and Systems Development',
+            'Data Structures and Algorithms',
+            'Theory of Computation',
+            'Professional & Transfer Skills',
+          ].map((item, index) => (
+            <li
+              key={index}
+              style={{
+                marginBottom: '8px',
+                position: 'relative',
+                paddingLeft: '20px',
+                lineHeight: '1.4',
+              }}
+            >
+              <span
+                style={{
+                  position: 'absolute',
+                  left: '0',
+                  content: '""',
+                  width: '6px',
+                  height: '6px',
+                  backgroundColor: '#4CAF50',
+                  borderRadius: '50%',
+                  top: '8px',
+                }}
+              ></span>
+              {item}
+            </li>
+          ))}
         </ul>
       </span>
     ),
@@ -107,18 +276,58 @@ let timelineElements = [
     type: 'EDUCATION',
     description: (
       <span>
-        <h6>1st Year Computer Science Modules </h6>
-        <ul>
-          <li>Architecture and Networks</li>
-          <li>Databases</li>
-          <li>Fundamentals of Maths for Computing</li>
-          <li>Programming</li>
-          <li>Web Technologies</li>
-          <li>Software Design Principles</li>
+        <h6
+          style={{
+            color: '#2F4F4F',
+            marginBottom: '15px',
+            fontSize: '1.1rem',
+            fontWeight: '600',
+          }}
+        >
+          1st Year Computer Science Modules
+        </h6>
+        <ul
+          style={{
+            listStyle: 'none',
+            padding: '0 15px',
+            margin: '0',
+          }}
+        >
+          {[
+            'Architecture and Networks',
+            'Databases',
+            'Fundamentals of Maths for Computing',
+            'Programming',
+            'Web Technologies',
+            'Software Design Principles',
+          ].map((item, index) => (
+            <li
+              key={index}
+              style={{
+                marginBottom: '8px',
+                position: 'relative',
+                paddingLeft: '20px',
+                lineHeight: '1.4',
+              }}
+            >
+              <span
+                style={{
+                  position: 'absolute',
+                  left: '0',
+                  content: '""',
+                  width: '6px',
+                  height: '6px',
+                  backgroundColor: '#4CAF50',
+                  borderRadius: '50%',
+                  top: '8px',
+                }}
+              ></span>
+              {item}
+            </li>
+          ))}
         </ul>
       </span>
     ),
-    //buttonText: 'View Backend Projects',
     date: 'September 2020 - June 2024',
     icon: 'queens',
   },
@@ -129,11 +338,50 @@ let timelineElements = [
     type: 'EDUCATION',
     description: (
       <span>
-        <h6>A Level: </h6>
-        <ul>
-          <li>Computer Science (A)</li>
-          <li>Maths (A)</li>
-          <li>Technology (A*)</li>
+        <h6
+          style={{
+            color: '#2F4F4F',
+            marginBottom: '15px',
+            fontSize: '1.1rem',
+            fontWeight: '600',
+          }}
+        >
+          A Level:
+        </h6>
+        <ul
+          style={{
+            listStyle: 'none',
+            padding: '0 15px',
+            margin: '0',
+          }}
+        >
+          {['Computer Science (A)', 'Maths (A)', 'Technology (A*)'].map(
+            (item, index) => (
+              <li
+                key={index}
+                style={{
+                  marginBottom: '8px',
+                  position: 'relative',
+                  paddingLeft: '20px',
+                  lineHeight: '1.4',
+                }}
+              >
+                <span
+                  style={{
+                    position: 'absolute',
+                    left: '0',
+                    content: '""',
+                    width: '6px',
+                    height: '6px',
+                    backgroundColor: '#4CAF50',
+                    borderRadius: '50%',
+                    top: '8px',
+                  }}
+                ></span>
+                {item}
+              </li>
+            )
+          )}
         </ul>
       </span>
     ),
@@ -147,18 +395,56 @@ let timelineElements = [
     type: 'EDUCATION',
     description: (
       <span>
-        <h6>AS Level: </h6>
-        <ul>
-          <li>Computer Science (B) </li>
-          <li>Maths (A) </li>
-          <li>Technology (A) </li>
+        <h6
+          style={{
+            color: '#2F4F4F',
+            marginBottom: '15px',
+            fontSize: '1.1rem',
+            fontWeight: '600',
+          }}
+        >
+          AS Level:
+        </h6>
+        <ul
+          style={{
+            listStyle: 'none',
+            padding: '0 15px',
+            margin: '0',
+          }}
+        >
+          {['Computer Science (B)', 'Maths (A)', 'Technology (A)'].map(
+            (item, index) => (
+              <li
+                key={index}
+                style={{
+                  marginBottom: '8px',
+                  position: 'relative',
+                  paddingLeft: '20px',
+                  lineHeight: '1.4',
+                }}
+              >
+                <span
+                  style={{
+                    position: 'absolute',
+                    left: '0',
+                    content: '""',
+                    width: '6px',
+                    height: '6px',
+                    backgroundColor: '#4CAF50',
+                    borderRadius: '50%',
+                    top: '8px',
+                  }}
+                ></span>
+                {item}
+              </li>
+            )
+          )}
         </ul>
       </span>
     ),
     date: 'September 2018 - June 2019',
     icon: 'school',
   },
-
   {
     id: 6,
     title: 'GCSE',
@@ -166,18 +452,59 @@ let timelineElements = [
     type: 'EDUCATION',
     description: (
       <span>
-        <h6>GCSE: </h6>
-        <ul>
-          <li>Computer Science (A*) </li>
-          <li>English Literature (A*) </li>
-          <li>History (A*) </li>
-          <li>Maths(A*) </li>
-          <li>Physical Education (A*) </li>
-          <li>Religious Education (A*) </li>
-          <li>English Language (A) </li>
-          <li>Physics (A) </li>
-          <li>Technology (A) </li>
-          <li>French (B) </li>
+        <h6
+          style={{
+            color: '#2F4F4F',
+            marginBottom: '15px',
+            fontSize: '1.1rem',
+            fontWeight: '600',
+          }}
+        >
+          GCSE:
+        </h6>
+        <ul
+          style={{
+            listStyle: 'none',
+            padding: '0 15px',
+            margin: '0',
+          }}
+        >
+          {[
+            'Computer Science (A*)',
+            'English Literature (A*)',
+            'History (A*)',
+            'Maths(A*)',
+            'Physical Education (A*)',
+            'Religious Education (A*)',
+            'English Language (A)',
+            'Physics (A)',
+            'Technology (A)',
+            'French (B)',
+          ].map((item, index) => (
+            <li
+              key={index}
+              style={{
+                marginBottom: '8px',
+                position: 'relative',
+                paddingLeft: '20px',
+                lineHeight: '1.4',
+              }}
+            >
+              <span
+                style={{
+                  position: 'absolute',
+                  left: '0',
+                  content: '""',
+                  width: '6px',
+                  height: '6px',
+                  backgroundColor: '#4CAF50',
+                  borderRadius: '50%',
+                  top: '8px',
+                }}
+              ></span>
+              {item}
+            </li>
+          ))}
         </ul>
       </span>
     ),
